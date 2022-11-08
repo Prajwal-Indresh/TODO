6df3f669d3b4b862ef77474e4a5f029fe190d821
Prajwal I <prajwalindresh135@gmail.com>
2022-11-08 14:40:48 +0530

    front end v1.0

---
+++ Frontend_01/src/components/page/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function About() {
+	return (
+		<div className="text-center">
+			<h1>About</h1>
+			<p>This is todo list app version 1.0.0</p>
+		
+		 
+		<p>Hi! We are the team behind the todo list website.
+			We created this website to help people keep track of their to-do lists and get things done!
+		   we love to lead people to more productive and organized lives.</p>
+		   
+
+		</div>
+	)
+}
